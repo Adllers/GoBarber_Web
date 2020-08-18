137343723dc3dc3ea96a4b0b8a4818a4378fc738
Adller Simas <adller.eel.ufsc@gmail.com>
2020-08-17 22:29:50 -0300

    tests auth hook

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -97,9 +97,9 @@ export function useAuth(): AuthContextData {
     
     const context = useContext(AuthContext);
 
-    if(!context) {
-        throw new Error('useAuth must be used within an AuthProvider')
-    }
+    // if(!context) {
+    //     throw new Error('useAuth must be used within an AuthProvider')
+    // }
 
     return context;
 }
